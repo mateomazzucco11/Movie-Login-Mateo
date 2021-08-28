1e6efe2facf259e6a4ba3fd0c53fce3eb9e1a994
Mateo <mateomazzucco4@gmail.com>
2021-08-28 12:24:40 -0300

    Adding the endpoint of favorites

--- server/src/routes/movies.js
+++ server/src/routes/movies.js
@@ -52,7 +52,7 @@ app.post('/login', (req, res) => {
 
 app.get('/movies', (_,res) => {
 
-  dataBase.query('SELECT id, year, title, description, director, cast, image FROM movies WHERE del = false', (err,result) => {
+  dataBase.query('SELECT id, year, title, description, director, cast, image FROM movies WHERE del = ?', ['false'], (err,result) => {
     if(err){
       return res
         .status(500)
@@ -70,9 +70,7 @@ app.get('/movies', (_,res) => {
 
     return res
       .status(200)
-      .json({
-        result,
-      });
+      .send(result)
   });
   
 });
@@ -151,14 +149,75 @@ app.delete('/movies/:id', verifyToken, (req,res) => {
   })
 })
 
-/* app.get('/favorites', (req,res) => {
-  
+app.get('/favorites/:id', verifyToken, (req,res) => {
+  const { id } = req.params
+
+  dataBase.query('SELECT id, title, year, gender, description, director, cast, image, del FROM movies INNER JOIN favorites ON favorites.movie_id = movies.id WHERE favorites.user_id = ?', id, (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({
+          msg: 'There was a problem on the server',
+        })
+    }
+
+    return res
+    .status(200)
+    .json({
+      msg: 'Favorites',
+      result
+    })
+  })
 });
 
-app.post('/favorites', (req,res) => {
+app.post('/favorites', (req, res) => {
+  const { movie_id, user_id } = req.body;
 
+  dataBase.query('INSERT INTO favorites (movie_id, user_id) VALUES (?, ?)', [movie_id, user_id], (err, result) => {
+    if(err) {
+      return res
+        .status(500)
+        .json({
+          msg: 'It is already added from favorites',
+        })
+    }
+
+    if(result.affectedRows > 0) {
+      return res
+        .status(200)
+        .json({
+          msg:'Successfully added to favorites',
+        });
+    } 
+  })
+})
+
+app.delete('/favorites', verifyToken, (req, res) => {
+  const { user_id, movie_id } = req.body
+
+  dataBase.query('DELETE FROM favorites WHERE user_id = ? AND movie_id = ?', [user_id, movie_id], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({
+          msg: 'There was a problem on the server',
+        })
+    }
+
+    if(result.affectedRows > 0) {
+      return res
+        .status(200)
+        .json({
+          msg:'Removed from favorites successfully',
+        });
+    } else {
+      return res
+        .status(400)
+        .json({
+          msg: 'It is already removed from favorites',
+        });
+    };
+  });
 });
- */
-
 
 module.exports = app;
