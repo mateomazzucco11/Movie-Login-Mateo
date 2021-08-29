84ffdcc4c80428fd2ef3105fc8bc36ee58557258
Mateo <mateomazzucco4@gmail.com>
2021-08-29 18:45:28 -0300

    Adding the cors in the app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const index = require('./routes/index');
+const cors = require('cors')
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use('/', index);
 
