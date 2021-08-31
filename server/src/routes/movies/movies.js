d65d5b6e217bc47081d882e597b8ec75149c6940
Mateo <mateomazzucco4@gmail.com>
2021-08-31 17:00:43 -0300

    Handling movies endpoints

--- server/src/routes/movies/movies.js
+++ server/src/routes/movies/movies.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dataBase = require('../../helpers/db/connect');
-const verifyAdmin = require('../../middleware/verifyAdmin');
 const verifyToken = require('../../middleware/verifyToken');
 require('dotenv').config();
 
@@ -30,9 +29,53 @@ app.get('/movies', (_,res) => {
   
 });
 
-app.put('/movies/:id', [verifyToken, verifyAdmin], (req, res) => {
+app.post('/movies', verifyToken, (req,res) => {
+  const { title, year, gender, description, director, cast, image } = req.body;
+
+  if(req.admin === 'false' ) {
+    return res
+      .status(400)
+      .json({
+        msg: 'This user is not an administrator',
+      });
+  };
+
+  dataBase.query('INSERT INTO movies SET title = ?, year = ?, gender = ?, description = ?, director = ?, cast = ?, image = ?', [title, year, gender, description, director, cast, image], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({
+          msg: 'There was a problem on the server',
+        });
+    };
+
+    if(result.changedRows > 0) {
+      return res
+        .status(200)
+        .json({
+          msg:'Movie added successfully',
+        });
+    } else {
+      return res
+        .status(400)
+        .json({
+          msg: 'Could not be added',
+        });
+    };
+  })
+})
+
+app.put('/movies/:id', verifyToken, (req, res) => {
   const { title, year, gender, description, director, cast, image } = req.body;
   const { id } = req.params;
+
+  if(req.admin === 'false' ) {
+    return res
+      .status(400)
+      .json({
+        msg: 'This user is not an administrator',
+      });
+  };
 
   dataBase.query('UPDATE movies SET title = ?, year = ?, gender = ?, description = ?, director = ?, cast = ?, image = ? WHERE id = ?', [title, year, gender, description, director, cast, image, id], (err, result) => {
     if (err) {
@@ -59,8 +102,16 @@ app.put('/movies/:id', [verifyToken, verifyAdmin], (req, res) => {
   });
 });
 
-app.delete('/movies/:id', [verifyToken, verifyAdmin], (req,res) => {
+app.delete('/movies/:id', verifyToken, (req,res) => {
   const { id } = req.params;
+
+  if(req.admin === 'false' ) {
+    return res
+      .status(400)
+      .json({
+        msg: 'This user is not an administrator',
+      });
+  };
 
   dataBase.query('UPDATE movies SET del = ? WHERE id = ?', ['true', id], (err, result) => {
     if (err) {
